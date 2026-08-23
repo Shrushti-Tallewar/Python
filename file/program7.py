95b138f8cb57398565e2d5820a79a8c2af29aea5
Shrushti-Tallewar <tallewarshrushti5@gmail.com>
2026-08-24 00:00:08 +0530

    program

--- file/program7.py
+++ file/program7.py
@@ -2,9 +2,11 @@ series = list(map(int,input().split()))
 print(series)
 
 
-for i in range(len(series)):
-    for j in range ( i +1 ,len(series)):
-        if series[i]> series[j] :
-            series[i],series[j]= series[j],series[i]
+# for i in range(len(series)):
+#     for j in range ( i +1 ,len(series)):
+#         if series[i]> series[j] :
+#             series[i],series[j]= series[j],series[i]
 
+# print(series)
+series.sort()
 print(series)
